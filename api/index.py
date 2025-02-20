691ce6a7301d4742259b6d017441b1fbd7bd4cb2
rajeshrc12 <rajesh.charhajari@gmail.com>
2025-02-20 14:33:31 +0530

    cors error fixed

--- api/index.py
+++ api/index.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-### Create FastAPI instance with custom docs and openapi url
+# Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
+
+# Allow CORS from frontend URL (and development URLs)
+origins = [
+    "https://tinyclips.vercel.app/",
+    "http://localhost:3000/"
+]
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
