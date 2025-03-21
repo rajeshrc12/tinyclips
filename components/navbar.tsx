76dfcebb82a8d4bf46885f8c723aae6e93e6c090
rajeshrc12 <rajesh.charhajari@gmail.com>
2025-03-21 16:29:58 +0530

    db user creation added

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,31 +4,34 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import SignOut from "@/components/sign-out";
 import Link from "next/link";
+import { auth } from "@/auth";
 
-const Navbar = () => {
-  return (
-    <div className="shadow border-b flex justify-between p-2">
-      <div className="flex gap-4">
-        <Link href={"/"}>
-          <Button variant="ghost">Create</Button>
-        </Link>
-        <Link href={"/videos"}>
-          <Button variant="ghost">Videos</Button>
-        </Link>
+const Navbar = async () => {
+  const session = await auth();
+  if (session?.user?.image)
+    return (
+      <div className="shadow border-b flex justify-between p-2">
+        <div className="flex gap-4">
+          <Link href={"/"}>
+            <Button variant="ghost">Create</Button>
+          </Link>
+          <Link href={"/videos"}>
+            <Button variant="ghost">Videos</Button>
+          </Link>
+        </div>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Avatar>
+              <AvatarImage src={session?.user?.image} alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </PopoverTrigger>
+          <PopoverContent className="w-30">
+            <SignOut />
+          </PopoverContent>
+        </Popover>
       </div>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </PopoverTrigger>
-        <PopoverContent className="w-30">
-          <SignOut />
-        </PopoverContent>
-      </Popover>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
