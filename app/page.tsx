ab7e76a68ec8b0fa79784d7608ca1d00811eaf14
rajeshrc12 <rajesh.charhajari@gmail.com>
2025-03-21 09:27:15 +0530

    shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,70 +1,8 @@
-"use client";
-import { useState } from "react";
-import axios from "axios";
-
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+import { Button } from '@/components/ui/button'
+import React from 'react'
 
 export default function Home() {
-  const [text, setText] = useState("");
-  // const [videoUrl, setVideoUrl] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  const handleSubmit = async (e: { preventDefault: () => void; }) => {
-    e.preventDefault();
-    setLoading(true);
-
-    try {
-      const response = await axios.post(`${API_URL}/items/`, 
-        {
-          "name": "Test Item", 
-          "description": "This is a test"
-        },
-    )
-    console.log(response.data)
-    } catch (error) {
-      console.error("Error fetching video:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
-      <h1 className="text-3xl font-bold mb-6">Next.js Video Generator</h1>
-      <form onSubmit={handleSubmit} className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="Enter text here"
-          className="w-full p-2 border border-gray-300 rounded mb-4"
-          required
-        />
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition"
-        >
-          Generate Video
-        </button>
-      </form>
-      {loading && (
-        <div className="mt-4">
-          <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-        </div>
-      )}
-      {/* {videoUrl && (
-        <div className="mt-6 bg-white p-4 rounded-lg shadow-md w-full max-w-md">
-          <h2 className="text-xl font-bold mb-2">Generated Video:</h2>
-          <video controls width="100%" height="auto">
-            <source src={videoUrl} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-      )} */}
-      <button onClick={async()=>{
-        const response = await axios.get(`${API_URL}/`);
-        console.log(response.data)
-      }}>Get method</button>
-    </div>
-  );
+    <div><Button>Click</Button></div>
+  )
 }
